03de8af95c262f913787efe3e9f29e14b1382802
a_cpp_coder <docdaodoi1@gmail.com>
2023-09-21 23:58:07 +0700

    Yes, go straight, no need to worry or care about other things

---
+++ boolean.c
@@ -0,0 +1,44 @@
+// No need to care about this for Now - Go straight forward, chac gi, cu di xem the nao da
+#include <stdio.h>
+
+int main(void){
+    long numb;
+    long sum = 0L;  // careful here, not waste time at learning at all
+    _Bool input_is_good;
+
+    printf("Please enter an integer to be summed");
+    printf("(q to quit): ");
+    input_is_good = (scanf("%ld", &numb));
+    do {
+        // input_is_good = (scanf("%ld", numb) == 1);  // very clear that you are amateur, C's scanf() does need the 
+                                                    // address of the variables to put the values in, not like C++ huh ?
+                                                    // but the compilers(gcc, clang) does show that you match the wrong type with warnings
+        sum += numb;
+        printf("Enter the next integer(q to quit): ");
+        input_is_good = (scanf("%ld", &numb));
+    } while(input_is_good);
+
+    printf("\nThe sum of your input integer is: %ld \n", sum);
+
+    return 0;
+}
+// boolean.c -- using a _Bool variable
+/*
+#include <stdio.h>
+int main(void)
+{
+long num;
+long sum = 0L;
+_Bool input_is_good;
+printf("Please enter an integer to be summed ");
+printf("(q to quit): ");
+input_is_good = (scanf("%ld", &num) == 1);
+while (input_is_good)
+{
+sum = sum + num;
+printf("Please enter next integer (q to quit): ");
+input_is_good = (scanf("%ld", &num) == 1);
+}
+printf("Those integers sum to %ld.\n", sum);
+return 0;
+}*/
