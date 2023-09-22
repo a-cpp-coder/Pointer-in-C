87f3f91d211e91acfaec7e0cb362bd409a2229ae
a_cpp_coder <docdaodoi1@gmail.com>
2023-09-22 17:30:07 +0700

    Trust in yourself, when learning and working, Living

--- Pointer_operators.cpp
+++ Pointer_operators.cpp
@@ -6,9 +6,17 @@ int main(void) {
     int* a_pointer;
 
     a_pointer = &count;
-    x = *a_pointer;
+    // x = (*a_pointer)++;     // to be careful about post-increment, may need use this in separate statement
+    x = ++*a_pointer;          // weird without the parentheses
 
-    printf("%d\n", x);
+    printf("%d %d\n", x, count);
+
+    // testing something really dangerous but to see if &count is a pointer or not
+    int z = 7;
+    // &count = &z;    // yeah, it's not, the book is wrong, trust in yourself
+    *a_pointer = *(&z); // how to modify(access) value of data item using pointer
+
+    printf("%d %d\n", x, count);
 
     return 0;
 }
