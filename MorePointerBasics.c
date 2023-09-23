5a46a765bfb41eeb001c3e7eaba69f403e9156b7
a_cpp_coder <docdaodoi1@gmail.com>
2023-09-23 14:26:46 +0700

    Code, feel it more, love more

---
+++ MorePointerBasics.c
@@ -0,0 +1,18 @@
+// Code, feel it more, love more
+
+#include <stdio.h>
+
+int main(void) {
+    char c = 'a';
+    char* char_pointer = &c;
+
+    printf("%c %c\n", c, *char_pointer);
+
+    c = 'b';
+    printf("%c %c\n", c, *char_pointer);
+
+    *char_pointer = 'c';
+    printf("%c %c\n", c, *char_pointer);
+
+    return 0;
+}
