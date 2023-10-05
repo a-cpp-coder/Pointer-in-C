f4028d5f426ab947e68555618667582ba65527cf
a_cpp_coder <docdaodoi1@gmail.com>
2023-10-05 23:58:19 +0700

    Add 2 possible practical ways to optimize the code, going to do it

--- FizzBuzz_v_Paralleling.c
+++ FizzBuzz_v_Paralleling.c
@@ -11,3 +11,10 @@ Print the result - trouble  here, may need locking/lazy eval
 -> read the parallel adder in Lec 6/slide 19/16-bit parallel adder
 no need any locking at all!
 the carry out bit of the current 4-bit adder feed directly into (as carry in bit) the next 4-bit adder
+'
+// The above approach is just my idea how to do it in parallelism
+/* In practical, there are at least 2 ways to do it:
+    - OpenMP parallelization
+    - FPGA
+
+*/
